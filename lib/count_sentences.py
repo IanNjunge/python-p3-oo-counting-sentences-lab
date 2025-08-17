043bb1c498a3e2b63905ee33edf11319559a9a5f
IanNjunge <ian.njunge@student.moringaschool.com>
2025-08-17 15:20:40 +0300

    Passes Tests

--- lib/count_sentences.py
+++ lib/count_sentences.py
@@ -1,4 +1,40 @@
 #!/usr/bin/env python3
 
 class MyString:
-  pass
+    def __init__(self, value = ""):
+      self.value = value
+    
+    @property
+    def value(self):
+        return self._value
+    
+    @value.setter
+    def value(self, new_value):
+       if isinstance(new_value, str):
+         self._value = new_value
+       else:
+          print("The value must be a string.")
+          
+    def is_sentence(self):
+        return self.value.endswith(".") 
+           
+    def is_question(self):
+        return self.value.endswith("?")
+      
+    def is_exclamation(self):
+        return self.value.endswith("!")
+      
+    def count_sentences(self):
+      if not self.value:
+        return 0  
+      
+      normalized = self.value.replace("?", ".")
+      normalized = normalized.replace("!", ".")
+      
+      parts = normalized.split(".")
+      
+      sentences = []
+      for p in parts:
+        if p !="": sentences.append(p)
+        
+      return len(sentences)
